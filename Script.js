eddab7ddecd559896cdefa47bb8d76d717bcbfec
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-13 02:45:33 +0000

    Jun 13, 2025, 2:45 AM

--- Script.js
+++ Script.js
@@ -2,3 +2,4 @@ console.log ("this means the JS is working")
 var UserName = prompt ("What's your name?")
 console.log (UserName)
 var UserWant = prompt ("Hello " +UserName+ "\nHow many seats would you like for your car?\n1 2 4 5 6 7 or 8?")
+var CarArray = [BACMono, MX5, ToughLuck, Mini, Falcon, Outlander, Estima]
