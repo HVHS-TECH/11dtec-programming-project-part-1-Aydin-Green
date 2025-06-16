50660220c732ee1e63323e965c685e5e2759526f
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-16 21:34:20 +0000

    Day 2 50% done

--- Script.js
+++ Script.js
@@ -2,4 +2,7 @@ console.log ("this means the JS is working")
 var UserName = prompt ("What's your name?")
 console.log (UserName)
 var UserWant = prompt ("Hello " +UserName+ "\nHow many seats would you like for your car?\n1 2 4 5 6 7 or 8?")
-var CarArray = [BAC_Mono, MX5, ToughLuck, Mini, Falcon, Outlander, Estima]
+var CarArray = ["BACMono", "MX5", "ToughLuck", "Mini", "Falcon", "Outlander", "Estima"]
+var CarWanted = UserWant-1
+var CarOffered = CarArray[CarWanted]
+console.log (CarOffered)
