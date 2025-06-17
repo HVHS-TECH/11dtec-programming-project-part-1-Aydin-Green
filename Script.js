56353e4265e740835b6ed0f3a41c559a1b8f7b56
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-17 23:59:55 +0000

    Jun 17, 2025, 11:59 PM

--- Script.js
+++ Script.js
@@ -6,19 +6,19 @@ var CarArray = ["BACMono", "MX5", "ToughLuck", "Mini", "Falcon", "Outlander", "E
 var CarWanted = UserWant-1
 var CarOffered = CarArray[CarWanted]
 console.log (CarOffered)
-if (CarOffered = "BACMono"){
+if (CarOffered == "BACMono"){
     console.log ("BAC Mono True")
     var BACMonoDays = Number(prompt ("you have chosen the BAC Mono this will cost you $10 per day how many days would you like to rent for?"))
     console.log ("BACMonoDays = " +BACMonoDays+ "")
     alert ("you have chosen to rent the BAC Mono for " +BACMonoDays+ " days we thank you for your partonage and hope you enjoy your new BAC Mono this will cost you $" +(BACMonoDays*10)+ "")
 }
-if (CarOffered = "MX5"){
+if (CarOffered == "MX5"){
     console.log ("MX5 = True")
     var MX5Days = Number(prompt ("you have chosen the MX5 this will cost you $20 per day how many days would you like to rent for?"))
     console.log ("MX5Days = " +MX5Days+ "")
     alert ("you have chosen to rent the MX5 for " +MX5Days+ " days we thank you for your partonage and hope you enjoy your new MX5 this will cost you $" +(MX5Days*20)+ "")
 }
-if (CarOffered = "ToughLuck"){
+if (CarOffered == "ToughLuck"){
     console.log ("ToughLuck True")
    alert ("Go away")
 }
