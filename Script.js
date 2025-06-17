739484a385f013b6d2903901a24f651d9d761be6
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-17 23:37:26 +0000

    Jun 17, 2025, 11:37 PM

--- Script.js
+++ Script.js
@@ -12,3 +12,15 @@ if (CarOffered = "BACMono"){
     console.log ("BACMonoDays = " +BACMonoDays+ "")
     alert ("you have chosen to rent the BAC Mono for " +BACMonoDays+ " days we thank you for your partonage and hope you enjoy your new BAC Mono this will cost you $" +(BACMonoDays*10)+ "")
 }
+if (CarOffered = "MX5"){
+    console.log ("MX5 = True")
+    var MX5Days = Number(prompt ("you have chosen the MX5 this will cost you $20 per day how many days would you like to rent for?"))
+    console.log ("MX5Days = " +MX5Days+ "")
+    alert ("you have chosen to rent the MX5 for " +MX5Days+ " days we thank you for your partonage and hope you enjoy your new MX5 this will cost you $" +(MX5Days*20)+ "")
+}
+if (CarOffered = "BACMono"){
+    console.log ("BAC Mono True")
+    var BACMonoDays = Number(prompt ("you have chosen the BAC Mono this will cost you $10 per day how many days would you like to rent for?"))
+    console.log ("BACMonoDays = " +BACMonoDays+ "")
+    alert ("you have chosen to rent the BAC Mono for " +BACMonoDays+ " days we thank you for your partonage and hope you enjoy your new BAC Mono this will cost you $" +(BACMonoDays*10)+ "")
+}
