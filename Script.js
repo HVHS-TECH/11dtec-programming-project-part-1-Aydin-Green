49c76d28ed5cde838c7cf3e1597b13ca1d4f5e86
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-18 00:04:49 +0000

    Jun 18, 2025, 12:04 AM

--- Script.js
+++ Script.js
@@ -22,3 +22,27 @@ if (CarOffered == "ToughLuck"){
     console.log ("ToughLuck True")
    alert ("Go away")
 }
+if (CarOffered == "Mini"){
+    console.log ("Mini = True")
+    var MiniDays = Number(prompt ("you have chosen the Mini this will cost you $40 per day how many days would you like to rent for?"))
+    console.log ("MiniDays = " +MiniDays+ "")
+    alert ("you have chosen to rent the Mini for " +MiniDays+ " days we thank you for your partonage and hope you enjoy your new Mini this will cost you $" +(MX5Days*40)+ "")
+}
+if (CarOffered == "Falcon"){
+    console.log ("Falcon = True")
+    var FalconDays = Number(prompt ("you have chosen the Falcon this will cost you $50 per day how many days would you like to rent for?"))
+    console.log ("FalconDays = " +FalconDays+ "")
+    alert ("you have chosen to rent the Falcon for " +FalconDays+ " days we thank you for your partonage and hope you enjoy your new Falcon this will cost you $" +(FalconDays*50)+ "")
+}
+if (CarOffered == "Outlander"){
+    console.log ("Outlander = True")
+    var OutlanderDays = Number(prompt ("you have chosen the Outlander this will cost you $60 per day how many days would you like to rent for?"))
+    console.log ("OutlanderDays = " +OutlanderDays+ "")
+    alert ("you have chosen to rent the Outlander for " +OutlanderDays+ " days we thank you for your partonage and hope you enjoy your new Outlander this will cost you $" +(MX5Days*60)+ "")
+}
+if (CarOffered == "MX5"){
+    console.log ("MX5 = True")
+    var MX5Days = Number(prompt ("you have chosen the MX5 this will cost you $20 per day how many days would you like to rent for?"))
+    console.log ("MX5Days = " +MX5Days+ "")
+    alert ("you have chosen to rent the MX5 for " +MX5Days+ " days we thank you for your partonage and hope you enjoy your new MX5 this will cost you $" +(MX5Days*20)+ "")
+}
