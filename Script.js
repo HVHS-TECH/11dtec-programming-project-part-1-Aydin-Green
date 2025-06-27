b1210a7c93a935a1144cd2b92e2b541bfc753636
Aydin-Green <23194ag@hvhs.school.nz>
2025-06-27 02:30:04 +0000

    Jun 27, 2025, 2:30 AM

--- Script.js
+++ Script.js
@@ -9,7 +9,7 @@ function start() {
             if (CarArray[index] != "Tough Luck") {
                 message = message + "With "+(index+1)+ "seats we have the " +CarArray[index]+ "\n"
             }}
-        alert ("The cars on offer are (in order of least to most seats) \n" +CarArray[index])
+        alert ("The cars on offer are (in order of least to most seats) \n" +message)
         var UserWant = prompt ("Hello " +UserName+ "\nHow many seats would you like for your car?\n1 2 4 5 7 or 8?")
         var CarWanted = UserWant-1
         var CarOffered = CarArray[CarWanted]
